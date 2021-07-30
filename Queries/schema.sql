648efea10a1c1f426a4ede5242d1c7fdf5ecbb88
kwporras <k.porras@yahoo.com>
2021-07-30 09:48:02 -0500

    Add retirement.csv and update querie files

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -35,13 +35,14 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE Dept_Emp (
+CREATE TABLE dept_emp (
   emp_no INT NOT NULL,
   dept_no VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
 CREATE TABLE Titles (
@@ -50,7 +51,10 @@ CREATE TABLE Titles (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES salaries (emp_no)
 );
 
-SELECT * FROM departments;
+SELECT * FROM salaries;
+
+
+
